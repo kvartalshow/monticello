a36d6f4e0d72fc4812cce5c1e0c2d079d45c6580
Pasha Grinchenko <kvartalshow@gmail.com>
2021-01-21 17:44:50 +0200

    fix bag with menu

--- src/js/burger-menu.js
+++ src/js/burger-menu.js
@@ -1,24 +1,40 @@
-// $('#sandwichmenu').on('click', function (e) {
-//   e.preventDefault();
-//   $('#sandwichmenu').toggleClass('active');
-//   $('.menu__box').toggleClass('active');
-// });
-// $('.burger__menu-a').on('click', function (e) {
-//   $('#sandwichmenu').toggleClass('active');
-//   $('.menu__box').toggleClass('active');
-// });
-// $('.burger__menu-item').on('click', function (e) {
-//   $('#sandwichmenu').toggleClass('active');
-//   $('.menu__box').toggleClass('active');
-// });
-
-document.querySelector('.tpl-hamburger').addEventListener('click', e => {
-  document.querySelector('.tpl-hamburger').classList.toggle('active');
-  document.querySelector('.menu__box').classList.toggle('active');
+$('#sandwichmenu').on('click', function (e) {
+  e.preventDefault();
+  $('#sandwichmenu').toggleClass('active');
+  $('.menu__box').toggleClass('active');
+});
+$('.burger__menu-a').on('click', function (e) {
+  $('#sandwichmenu').toggleClass('active');
+  $('.menu__box').toggleClass('active');
 });
 
-document.querySelector('.burger__menu-item').addEventListener('click', e => {
-  document.querySelector('.tpl-hamburger').classList.toggle('active');
-  document.querySelector('.menu__box').classList.toggle('active');
+$('.burger__menu-item').on('click', function (e) {
+  $('#sandwichmenu').toggleClass('active');
+  $('.menu__box').toggleClass('active');
 });
 
+$('.burger__a--1').on('click', function (e) {
+  $('#sandwichmenu').toggleClass('active');
+  $('.menu__box').toggleClass('active');
+});
+$('.burger__a--2').on('click', function (e) {
+  $('#sandwichmenu').toggleClass('active');
+  $('.menu__box').toggleClass('active');
+});
+$('.burger__a--3').on('click', function (e) {
+  $('#sandwichmenu').toggleClass('active');
+  $('.menu__box').toggleClass('active');
+});
+$('.burger__a--4').on('click', function (e) {
+  $('#sandwichmenu').toggleClass('active');
+  $('.menu__box').toggleClass('active');
+});
+// document.querySelector('.tpl-hamburger').addEventListener('click', e => {
+//   document.querySelector('.tpl-hamburger').classList.toggle('active');
+//   document.querySelector('.menu__box').classList.toggle('active');
+// });
+
+// document.querySelector('.burger__menu-item').addEventListener('click', e => {
+//   document.querySelector('.tpl-hamburger').classList.toggle('active');
+//   document.querySelector('.menu__box').classList.toggle('active');
+// });
